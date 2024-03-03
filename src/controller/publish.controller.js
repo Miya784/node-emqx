c7ff8427cfbcdc308fc9c5a0ecdc70e8966ddb2b
burhan <9burhanjehloh@gmail.com>
2024-03-03 23:09:52 +0700

    b1

--- src/controller/publish.controller.js
+++ src/controller/publish.controller.js
@@ -1,27 +1,22 @@
-
-const { post: emqxPost } = require("../services/emqx-api.js");
-const { Client } = require("../models/user.model.js");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-
+// auth.controller.js
+import { API } from '../services/emqx-api.js'; 
+import { Client } from '../models/user.model.js';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+// import { stack } from 'sequelize/types/utils.js';
 dotenv.config();
 
-const publish = async (req, res) => {
+export const publish = async (req, res) => {
   try {
     const { topic, payload } = req.body;
-
+    
     // Check if the Authorization header is present
     const token = req.headers.authorization;
+
     if (!token) {
-      return res.status(401).json({
-        error: 'Unauthorized. Token not provided.'
-      });
+      return res.status(401).json({ error: 'Unauthorized. Token not provided.' });
     }
-    const client = await Client.findOne({
-      where: {
-        client: topic
-      }
-    });
+    const client = await Client.findOne({where:{client:topic}});
     if (!client) {
       res.status(404).send("User not found.");
       return;
@@ -29,38 +24,60 @@ const publish = async (req, res) => {
     // Verify and decode the token
     jwt.verify(token.replace('Bearer ', ''), 'your_secret_key', (err, decoded) => {
       if (err) {
-        return res.status(401).json({
-          error: 'Unauthorized. Invalid token.'
-        });
+        return res.status(401).json({ error: 'Unauthorized. Invalid token.' });
       }
 
       // Example: Publish a message to a topic
-      emqxPost(process.env.API_PUBLISH,
-      // Adjust the endpoint based on your EMQX version
-      {
-        topic,
-        payload: JSON.stringify(payload),
-        qos: 0,
-        retain: false
-      }, {
-        headers: {
-          "Content-Type": "application/json"
+      API.post(
+        process.env.API_PUBLISH, // Adjust the endpoint based on your EMQX version
+        {
+          topic,
+          payload: JSON.stringify(payload),
+          qos: 0,
+          retain: false,
+        },
+        {
+          headers: { "Content-Type": "application/json" },
         }
-      }).then(publishResponse => {
+      )
+      .then((publishResponse) => {
         res.json(publishResponse.data);
-      }).catch(error => {
+      })
+      .catch((error) => {
         console.error('Error during publish:', error);
-        res.status(500).json({
-          error: 'Internal Server Error'
-        });
+        res.status(500).json({ error: 'Internal Server Error' });
       });
     });
   } catch (error) {
     console.error('Error during publish:', error);
-    return res.status(500).json({
-      error: 'Internal Server Error'
-    });
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 };
 
-module.exports = { publish };
+
+// export const topics = async (req , res ) => {
+//   try {
+//     const { topic ,typeClient} = req.body;
+
+//     API.post(
+//       process.env.API_TOPICS, // Adjust the endpoint based on your EMQX version
+//       {
+//         topic,
+//         typeClient
+//       },
+//       {
+//         headers: { "Content-Type": "application/json" },
+//       }
+//     )
+//     .then((publishResponse) => {
+//       res.json(publishResponse.data);
+//     })
+//     .catch((error) => {
+//       console.error('Error during publish:', error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     });
+//   }catch(error){
+//     console.error('Error status Client : ',error);
+//     res.status(500).json({error: 'Internal Server Error'});
+//   }
+// };
