68b16ef2d98c8dcfbce20eb78a74269a29fb2565
WonYus <lucy_oliv@outlook.com>
2024-03-04 01:24:34 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+import { config } from "./src/config/loadenv.js";
+import dbConfig from "./src/config/db.config.js";
 import express from "express";
 import bodyParser from "body-parser";
-import dbConfig from "./src/config/db.config.js";
 import authRoutes from "./src/routes/auth.routes.js";
 import publishRoutes from "./src/routes/publish.routes.js";
-import { config } from "./src/config/loadenv.js";
 
-const app = express();
+const app = express(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
